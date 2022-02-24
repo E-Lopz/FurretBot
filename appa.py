30be6de769ccaf974cea9de66b4da7da15607934
Emiliano Lopez <emiliano.lopez2404@gmail.com>
2022-02-24 15:00:09 -0500

    Added a basic stucture and the 'hola' command

---
+++ appa.py
@@ -0,0 +1,17 @@
+from http import client
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='uwu')
+
+client = discord.client()
+
+@client.event 
+async def on_ready():
+    print('Appa is now logged in as (0.user)'.format(client))
+
+@bot.command()
+async def hola(ctx):
+    await ctx.reply('Que pedo pinches puercas \*Sonido de Appa\*')
+    
+bot.run('OTQ2NDc4ODYyNjE3OTUyMjk3.YhfTIQ.XnNVnbI7qSbA_P_gGOImw0E7b64')
