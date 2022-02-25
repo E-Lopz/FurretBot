e54f22f7f66a6a6de75ca4d3381c5cc5c6db40a1
Emiliano Lopez <emiliano.lopez2404@gmail.com>
2022-02-24 23:56:01 -0500

    Added play function

--- appa.py
+++ appa.py
@@ -1,28 +1,38 @@
+## This bot needs a previous installation of FFMPEG.
 import discord
 import sys
 from discord.ext import commands
+import youtube_dl
 
-bot = commands.Bot(command_prefix='uwu ',help_command=None)
+client = commands.Bot(command_prefix='uwu ',help_command=None)
 
-@bot.command()
+@client.command()
 async def help(ctx):
     embed=discord.Embed(title="Lista de Comandos", description='<:Pablo0:782755700677541898>**Random**\nHola\n\n<:UwU:801886649469632562>**NSFW**\nTraps\n\n<:gabobb:787384850922995732>**Musica**\nTrabajando en eso\n\nRecuerda usar estos comandos despues del prefijo "uwu ". \nEspero haberte ayudado. <:like:783029416812609556>', color=0xFF5733)
     await ctx.send(embed=embed)
 
-@bot.command()
+@client.command()
 async def hola(ctx):
     await ctx.reply('Hola \*Sonido de Appa\*')
 
-@bot.command()
-async def kill(ctx):
+@client.command()
+async def morir(ctx):
     await ctx.reply('Ciao')
     sys.exit("Appa has left the chat")
 
-@bot.command()
+@client.command()
 async def traps(ctx):
      embed=discord.Embed(title="Sike!", color=0xFF5733)
      embed.set_image(url="https://c.tenor.com/synaMy5G9VoAAAAC/appa-avatar.gif")
      await ctx.send(embed=embed)
+
+@client.command()
+async def play(ctx, url : str):
+    voiceChannel = ctx.author.voice.channel
+    await voiceChannel.connect()
+    vc = discord.utils.get(client.voice_clients,guild = ctx.guild)
+    vc.play(discord.FFmpegPCMAudio(source='song.mp3'))
+
     
     
-bot.run('OTQ2NDc4ODYyNjE3OTUyMjk3.YhfTIQ.mrJqA1O2084PQInBAUYixUj1NHc')
+client.run('OTQ2NDc4ODYyNjE3OTUyMjk3.YhfTIQ.mrJqA1O2084PQInBAUYixUj1NHc')
