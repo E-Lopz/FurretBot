3ee6dd8a4d22cf101116bbba02dc1650e0fde0d7
Emiliano Lopez <emiliano.lopez2404@gmail.com>
2022-02-24 19:03:03 -0500

    Changed help formatting

--- appa.py
+++ appa.py
@@ -1,17 +1,20 @@
-from http import client
 import discord
+import sys
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='uwu')
+bot = commands.Bot(command_prefix='uwu ',help_command=None)
 
-client = discord.client()
-
-@client.event 
-async def on_ready():
-    print('Appa is now logged in as (0.user)'.format(client))
+@bot.command()
+async def help(ctx):
+    await ctx.send('Lista de Comandos\n <:Pablo0:782755700677541898>Random\n-hola: Appa te manda un saludote.\n<:UwU:801886649469632562>NSFW\n-traps\n<:gabobb:787384850922995732>Musica\n-trabajando en eso\nRecuerda usar estos comandos despues del prefijo "uwu " espero haberte ayudado <:like:783029416812609556>')
 
 @bot.command()
 async def hola(ctx):
-    await ctx.reply('Que pedo pinches puercas \*Sonido de Appa\*')
+    await ctx.reply('Hola \*Sonido de Appa\*')
+
+@bot.command()
+async def kill(ctx):
+    await ctx.reply('Ciao')
+    sys.exit("Appa has left the chat")
     
 bot.run('OTQ2NDc4ODYyNjE3OTUyMjk3.YhfTIQ.mrJqA1O2084PQInBAUYixUj1NHc')
